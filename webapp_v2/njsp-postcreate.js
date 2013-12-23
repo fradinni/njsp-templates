68b657bf4245a31c3d228951b7764a55ac29ee76
Nicolas FRADIN <fradinni@gmail.com>
2013-12-23 12:34:21 +0100

    update post create script for webapp v2

--- webapp_v2/njsp-postcreate.js
+++ webapp_v2/njsp-postcreate.js
@@ -1,7 +1,15 @@
+var fs = require('fs');
 var spawn = require('child_process').spawn;
 
 module.exports = function (options, callback) {
-  console.log('Webapp template post creation script...');
+  console.log('Execute post creation script');
+
+  // If heroku
+  if(options.heroku) {
+    // Generate Procfile
+    fs.writeFileSync(options.projectPath+'/Procfile', 'web: node server/server.js\n');
+  }
+
   console.log('Installing project dependencies...');
   // Execute npm install...
   var install = spawn('npm', ['install'], {cwd: options.projectPath});
