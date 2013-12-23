4f1080fcaf983880eb3f540f02acb0e2544bcc21
Nicolas FRADIN <fradinni@gmail.com>
2013-12-23 12:05:31 +0100

    update post create script for webapp v2

--- webapp_v2/njsp-postcreate.js
+++ webapp_v2/njsp-postcreate.js
@@ -2,25 +2,12 @@ var spawn = require('child_process').spawn;
 
 module.exports = function (options, callback) {
   console.log('Webapp template post creation script...');
-
+  console.log('Installing project dependencies !');
   // Execute npm install...
   var error = false;
   var install = spawn('npm', ['install'], {cwd: options.projectPath});
 
-  install.stderr.on('data', function (data) {
-    error = true;
-    console.log(data.toString());
-  });
-
-  install.stdout.on('data', function (data) {
-    console.log('\033[32m'+data.toString()+'\033[30m');
-  });
-
   install.on('close', function () {
-    if(error) {
-      console.log('Error during initialisation !');
-      return callback(error);
-    }
     return callback(null);
   });
 };
